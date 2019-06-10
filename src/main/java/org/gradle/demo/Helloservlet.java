534964620a85200ef810423d89c0deba83d7b8bf
pal.axelor <pal.axelor@gmail.com>
2019-06-10 17:48:55 +0530

    java web app is added

---
+++ src/main/java/org/gradle/demo/Helloservlet.java
@@ -0,0 +1,24 @@
+package org.gradle.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Helloservlet", urlPatterns = {"prashant"}, loadOnStartup = 1) 
+public class Helloservlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.getWriter().print("Hello, World!");  
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) name = "World";
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response); 
+    }
+}
